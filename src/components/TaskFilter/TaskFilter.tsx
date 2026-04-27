401553388ccc6085d1cd1e454e618760a0dd45ba
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 12:01:06 -0400

    added UI rendering

--- src/components/TaskFilter/TaskFilter.tsx
+++ src/components/TaskFilter/TaskFilter.tsx
@@ -6,7 +6,23 @@ function TaskFilter({onFilterChange}: TaskFilterProps) {
     
     const handlePriorityChange = (e: React.ChangeEvent<HTMLSelectElement>) => 
         onFilterChange({priority: e.target.value as any})
-    
+    return (
+        <div>
+            <select onChange={handleStatusChange}>
+                <option value="">All Statues</option>
+                <option value="pending ">Pending</option>
+                <option value="in-progress">In Progress</option>
+                <option value="completed">Completed</option>
+            </select>
+
+            <select onChange={handlePriorityChange}>
+                <option value="">All Priorities</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+        </div>
+    )
 };
 
 
