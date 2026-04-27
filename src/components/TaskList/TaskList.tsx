53493985e232786c8f25c2698dcf57c407c33e12
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 11:37:33 -0400

    added conditional rendering

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,30 +1,44 @@
 import TaskItem from '../TaskItem/TaskItem';
-import type {TaskListProps, TaskStatus} from '../../types';
-import {useState} from 'react';
+import type { TaskListProps} from '../../types';
+import { useState } from 'react';
 
-function TaskList({tasks, onStatusChange, onDelete}: TaskListProps) {
+function TaskList({ tasks, onStatusChange, onDelete }: TaskListProps) {
     // SET STATE
     const [filter, setFilter] = useState('all');
 
     // FILTERING LOGIC
-    const filteredTasks = filter === 'all' 
-    ? tasks 
-    : tasks.filter((task) => task.status === filter)
+    const filteredTasks = filter === 'all'
+        ? tasks
+        : tasks.filter((task) => task.status === filter)
 
     // RENDER UI FOR TASKLIST
-    return(
+    return (
         <div>
             <h2>Task List</h2>
 
             {/* FILTER UI */}
-            <select 
-            onChange={(e) => setFilter(e.target.value)}
+            <select
+                onChange={(e) => setFilter(e.target.value)}
             >
                 <option value="all-statues">All Statues</option>
                 <option value="pending">Pending</option>
                 <option value="in-progress">In Progress</option>
                 <option value="completed">Completed</option>
             </select>
+
+            {/* CONDITIONAL RENDERING */}
+            {filteredTasks.length === 0 ? (
+                <p>No Tasks Found</p>
+            ) : (
+                filteredTasks.map((task) => (
+                    <TaskItem
+                        key={task.id}
+                        task={task}
+                        onStatusChange={onStatusChange}
+                        onDelete={onDelete}
+                    />
+                ))
+            )}
         </div>
     )
 
