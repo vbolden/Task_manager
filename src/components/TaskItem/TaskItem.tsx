a875d4901d1aaf46d567dcc4b0844ecb7443e68c
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 14:05:49 -0400

    changed UI structure and added classNames for styling

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,29 +1,35 @@
 import type { TaskItemProps } from '../../types';
-import type { TaskStatus }  from '../../types';
+import type { TaskStatus } from '../../types';
 
-function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
+function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
     return (
-        <div>
-            <div>
+        <div className='task-card'>
+            <div className='task-header'>
                 <h3>{task.title}</h3>
-                <p>{task.description}</p>
-                <span>Priority: {task.priority} </span>
+                {/* STATUS CHANGE */}
+                <div className="status-actions">
+                    <select
+                        value={task.status}
+                        onChange={(e) =>
+                            onStatusChange(task.id, e.target.value as TaskStatus)
+                        } >
+                        <option value="pending">Pending</option>
+                        <option value="in-progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                    </select>
+                    {/* DELETE BUTTON */}
+                    <button onClick={() => onDelete(task.id)}>Delete</button>
+                </div>
             </div>
 
-            {/* STATUS CHANGE */}
-            <div className={`status ${task.status}`}>
-                <select
-                value={task.status}
-                onChange={(e) =>
-                    onStatusChange(task.id, e.target.value as TaskStatus)
-                } >
-                    <option value="pending">Pending</option>
-                    <option value="in-progress">In Progress</option>
-                    <option value="completed">Completed</option>
-                </select>
-                {/* DELETE BUTTON */}
-                <button onClick={() => onDelete(task.id)}>Delete</button>
-            </div>
+            <p>{task.description}</p>
+
+            <div className='task-footer'></div>
+            <span className={`priority ${task.priority}`}>
+                Priority: {task.priority} </span>
+            <span className='due-date'>
+                Due: {task.dueDate}</span>
+
         </div>
     )
 }
