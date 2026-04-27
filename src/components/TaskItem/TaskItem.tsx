4cd845a03b9bfd764dc127a9820c63d4ca3cb3fb
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 10:59:52 -0400

    added UI rendering for task item

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,7 +1,30 @@
-import type {TaskItemProps} from '../../types';
+import type { TaskItemProps } from '../../types';
+import type { TaskStatus }  from '../../types';
 
-function TaskItem() {
-    
+function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
+    return (
+        <div>
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+
+            <span>Priority: {task.priority} </span>
+
+            {/* STATUS CHANGE */}
+            <div className={`status ${task.status}`}>
+                <select
+                value={task.status}
+                onChange={(e) =>
+                    onStatusChange(task.id, e.target.value as TaskStatus)
+                } >
+                    <option value="pending">Pending</option>
+                    <option value="in-progress">In Progress</option>
+                    <option value="completed">Completed</option>
+                </select>
+                {/* DELETE BUTTON */}
+                <button onClick={() => onDelete(task.id)}>Delete</button>
+            </div>
+        </div>
+    )
 }
 
 export default TaskItem;
