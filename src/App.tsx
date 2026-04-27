3ed85eb16509927f64a829f3ba08a8141c82b886
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 13:27:46 -0400

    added component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { useState } from 'react'
+import TaskList from './components/TaskList/TaskList'
+import TaskFilter from './components/TaskFilter/TaskFilter'
+import type { Task, TaskStatus } from './types'
 
 function App() {
+  
 
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
 
