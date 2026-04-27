3b35aa46a3650723764f90999b5135f30a2ab7f6
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-27 13:18:43 -0400

    removed all option from select element, and removed list header

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -4,10 +4,11 @@ import type { TaskStatus }  from '../../types';
 function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
     return (
         <div>
-            <h3>{task.title}</h3>
-            <p>{task.description}</p>
-
-            <span>Priority: {task.priority} </span>
+            <div>
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+                <span>Priority: {task.priority} </span>
+            </div>
 
             {/* STATUS CHANGE */}
             <div className={`status ${task.status}`}>
@@ -16,7 +17,6 @@ function TaskItem({task, onStatusChange, onDelete}: TaskItemProps) {
                 onChange={(e) =>
                     onStatusChange(task.id, e.target.value as TaskStatus)
                 } >
-                    <option value="all">All</option>
                     <option value="pending">Pending</option>
                     <option value="in-progress">In Progress</option>
                     <option value="completed">Completed</option>
